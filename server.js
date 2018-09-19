7322cb9691a0351af42f92613f29567628cf44a9
Rupasa Karmakar <35370479+rk2406@users.noreply.github.com>
2018-09-19 08:23:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,17 +5,86 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fire-demo-bd419.firebaseio.com"
 });
-    var ref = admin.app().database().ref();
-    var sensors = ref.child('server/users');
-    let globe,final;
-    setInterval(function(){
-        globe=Math.floor((Math.random()*100)+1);
-        final=Math.floor((Math.random()*100)+1);
+const express=require('express')
+const app=express();
+
+const http=require('http')
+
+const socketio=require('socket.io')
+var SerialPort = require("serialport")
+var serialPort = new SerialPort('COM22',
+{   
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1, 
+  parser: new SerialPort.parsers.Readline("\n"),
+  flowControl: false
+});
+
+const server=http.createServer(app)
+const io=socketio(server)
+var receivedData="";
+let sendData="";
+let obj={};
+let arr=[];
+var ref = admin.app().database().ref();
+var sensors = ref.child('server/users');
+serialPort.on("open", function () {
+    console.log('comm open');
+    serialPort.on('data', function(data) {
+      
+      receivedData += data.toString();
+      
+      if(receivedData.indexOf('$')>0)
+      {
+        if(receivedData[0]!='#')
+        receivedData='#'+receivedData;
+        sendData=receivedData.split('$')[0];
+        // console.log(sendData);
+        obj.one=sendData.split('#')[2];
+        obj.two=(sendData.split('#')[3]);
+        obj.three=(sendData.split('#')[4]);
+        obj.four=(sendData.split('#')[5]);
+        if(obj.one== undefined || obj.two== undefined||obj.three== undefined||obj.four== undefined )
+        {
+          obj.one=-1;
+          obj.two=-1;
+          obj.three=-1;
+          obj.four=-1;
+        }
+        console.log("Send Data "+obj.one+" "+obj.two+" "+obj.three+" "+obj.four);
+        receivedData="";
         sensors.push({
-            globe:globe,
-            final:final
+            sensor_1:parseFloat(obj.one),
+            sensor_2:parseFloat(obj.two),
+            sensor_3:parseFloat(obj.three),
+            sensor_4:parseFloat(obj.four)
            });    
-    },1000);
+      }
+    });
+});
+
+serialPort.on('error', function(err) {
+    console.log('Error: ', err.message);
+    // console.error(err);
+  })
+  
+  io.on('connection',(socket)=>{
+    console.log('New socket formed from '+socket.id+" client");
+    socket.emit('connected');
+    setInterval(()=>{
+      socket.emit('update', obj);
+    },100)
+     
+  })
+  
+  server.listen(4000,()=>{
+      console.log("server started")
+  })
+
+
+    
     
     
 
